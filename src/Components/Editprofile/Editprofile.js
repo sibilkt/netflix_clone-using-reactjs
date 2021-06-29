1bbfe17ab09437bb20bb33c6d97d14dd7c09abd5
Sibil kt <jdoit000@gmail.com>
2021-06-29 10:20:06 +0530

    Delete Account option added to Editprofile Components

--- src/Components/Editprofile/Editprofile.js
+++ src/Components/Editprofile/Editprofile.js
@@ -1,21 +1,46 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import './Editprofile.css'
 import Navbar from '../Navbar/Navbar'
 import { AuthContext, FirebaseContext } from '../../Context/firebaseContext'
-import { useHistory } from 'react-router-dom';
+import { Redirect, useHistory } from 'react-router-dom';
 
 function Editprofile() {
-    const {user} = useContext(AuthContext);
-    const {fireBase} = useContext(FirebaseContext)
-    const history=useHistory()
-    const signout=(e)=>{
+    const { user, setUser } = useContext(AuthContext);
+    const { fireBase } = useContext(FirebaseContext)
+    const history = useHistory()
+
+    const deleteUser = (e) => {
+        e.preventDefault()
+        const confirm = window.confirm("Do you realy want to delete your Account?")
+        if (confirm === true) {
+            user.delete().then(() => {
+                setUser('')
+                alert('Your acoount has been deleted. Please Signup again to Use Netflix');
+            }).catch((error) => {
+                console.log(error.message);
+            })
+            
+        }else{
+            history.push('/profile')
+        }
+    }
+
+
+    const signout = (e) => {
         e.preventDefault();
         fireBase.auth().signOut().then(() => {
             history.push('/login');
-          }).catch((error) => {
+        }).catch((error) => {
             alert(error.message)
-          });
+        });
     }
+    // useEffect(() => {
+    //     fireBase.firestore().collection(`${user.email}`).get().then((snapshort)=>{
+    //         snapshort.forEach((doc)=>{
+    //             console.log(doc.id,"=>",doc.data());
+    //         })
+    //     })
+    // }, [])
     return (
         <div className="editprofile">
             <Navbar />
@@ -23,16 +48,19 @@ function Editprofile() {
                 <h1>Edit Profile</h1>
                 <div className="profile_info">
                     <img className="avatars"
-                    src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="Avatar" />
+                        src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="Avatar" />
                     <div className="details">
                         <h2>{user.email}</h2>
                         <div className="more_data">
                             <button className="signout"
-                            onClick={signout}>Sign Out</button>
+                                onClick={deleteUser}>Delete Your Account</button>
+                            <button className="signout"
+                                onClick={signout}>Sign Out</button>
                         </div>
 
                     </div>
                 </div>
+                <h1></h1>
             </div>
         </div>
     )
